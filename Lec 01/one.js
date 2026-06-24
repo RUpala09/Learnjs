17076df5a1c6b92789c896aecd792858c38e9fe5
Raj Upala <rajupala005@gmail.com>
2026-06-24 15:28:26 +0530

    add new operator

--- Lec 01/one.js
+++ Lec 01/one.js
@@ -1,6 +1,14 @@
 
-let a = 1;
+// let a = 1;
+// let b = 5;
+
+// a *= 5;
+// console.log("a =",a);
+
+//Comparison Operators :
+
+let a = 5;
 let b = 5;
 
-a *= 5;
-console.log("a =",a);
+console.log(a != b);
+
