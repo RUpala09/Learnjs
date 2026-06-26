8b96f145ca399710bc0aff35fc66e460f3e9a06d
Raj Upala <rajupala005@gmail.com>
2026-06-26 13:08:58 +0530

    add new loop condepts

---
+++ Lec 01/Three.js
@@ -0,0 +1,13 @@
+// for loop 
+
+// for(let i=1; i<=5; i++){
+//     console.log(i)
+// }
+let sum = 0;
+
+for(let i=1; i<=5; i++){
+    sum = sum + i;
+}
+
+console.log(sum)
+console.log("loop ended.")
