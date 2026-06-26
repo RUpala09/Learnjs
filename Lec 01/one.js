7eaa28965dba69d976725edbac38a04d0e696d58
Raj Upala <rajupala005@gmail.com>
2026-06-26 12:33:21 +0530

    doing some practice questions

--- Lec 01/one.js
+++ Lec 01/one.js
@@ -1,6 +1,28 @@
-//Ternary Operators
+// get num using prompt 
 
-let age = 15;
+// let Num = prompt("Enter Num :");
 
-let result = age >= 18 ? "adult" : "Not adult";
-console.log(result);
+// if(Num % 4 === 0){
+//     console.log(Num,"is multiple of 4.");
+// }else{
+//     console.log(Num,"is not multiple of 4.");
+// }
+
+// add grade accordign to marks ranges.
+
+let marks = prompt("Enter Your Marks :");
+console.log(marks);
+
+if(marks >= 90 && marks <= 100){
+    console.log("Grade A.");
+}else if(marks >= 70 && marks <= 89){
+    console.log("Grade B");
+}else if(marks >= 60 && marks <= 69){
+    console.log("Grade C");
+}else if(marks >= 50 && marks <= 59){
+    console.log("Grade D");
+}else if(marks >= 0 && marks <= 49){
+    console.log("Grade F");
+}else{
+    console.log("Invaild marks number");
+}
