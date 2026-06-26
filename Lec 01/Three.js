deb97a2157429a46c3089f015b9a2eb0dc2a1598
Raj Upala <rajupala005@gmail.com>
2026-06-26 14:00:07 +0530

    learn loops

--- Lec 01/Three.js
+++ Lec 01/Three.js
@@ -3,11 +3,30 @@
 // for(let i=1; i<=5; i++){
 //     console.log(i)
 // }
-let sum = 0;
 
-for(let i=1; i<=5; i++){
-    sum = sum + i;
-}
+// let sum = 0;
 
-console.log(sum)
-console.log("loop ended.")
+// for(let i=1; i<=5; i++){
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+// console.log("loop ended.");
+
+//while loop
+
+// let i=1;
+// while(i <= 5){
+//     console.log("i=",i);
+//     i++;
+// }
+
+// console.log("loop end.");
+
+//do-while loop
+
+let i=1;
+do{
+    console.log("i=",i);
+    i++;
+}while(i <= 5);
