4442cfc6cb07dc103905297cf1367c6423ce6600
Raj Upala <rajupala005@gmail.com>
2026-06-27 16:03:31 +0530

    doing some Qs

--- Lec 01/four.js
+++ Lec 01/four.js
@@ -11,7 +11,47 @@
 
 // => array indices(index)
 
-let marks = [12,14,15,10,18];
-console.log(marks[0]);
-marks[3]=0;
-console.log(marks); //(here marks values is mutable means changeble so upper line changes happens.)
+// let marks = [12,14,15,10,18];
+// console.log(marks[0]);
+// marks[3]=0;
+// console.log(marks); //(here marks values is mutable means changeble so upper line changes happens.)
+
+// => loop in arrays
+
+// let heros = ["ironman", "spiderman", "batman","heman","thor"];
+
+// for(let i=0; i < heros.length; i++){
+//     console.log(heros[i]);
+// }
+// console.log("loop ended.");
+
+//using for of
+
+// let studentsNames = ["raj","ram","yash","jash","jigar"];
+
+// for(let student of studentsNames){
+//     console.log(student);
+// }
+
+// Qs
+
+// let marks =[85,97,44,37,76,60];
+// let sum = 0;
+
+// for(let val of marks){
+//     sum += val;
+// }
+
+// let average = sum / marks.length;
+// console.log(`Total of average marks = ${average}`);
+
+// Qs 
+
+let price = [250,645,300,900,50];
+
+for(let i=0; i<price.length; i++){
+    let offer = price[i] / 10;
+    price[i] -= offer;
+}
+
+console.log(`after appply price is ${price}`);
