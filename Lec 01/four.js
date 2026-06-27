4d4174bb4f5e5369be114b836ae3495ca1d51d15
Raj Upala <rajupala005@gmail.com>
2026-06-27 15:02:12 +0530

    basic arrays perfom it

---
+++ Lec 01/four.js
@@ -0,0 +1,10 @@
+// arrays (collection of items)
+
+let heros = ["ironman", "spiderman", "batman","heman","thor"];
+console.log(heros);
+
+let marks = [12,14,15,16,19]; //(out of 20)
+console.log(marks);
+
+let info = ["rahul",12,"Delhi"];
+console.log(info);
