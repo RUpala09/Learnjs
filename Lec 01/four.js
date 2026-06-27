f69a9449886a67d99528c99bc467c84275706be9
Raj Upala <rajupala005@gmail.com>
2026-06-27 15:12:14 +0530

    add array indices

--- Lec 01/four.js
+++ Lec 01/four.js
@@ -1,10 +1,17 @@
 // arrays (collection of items)
 
-let heros = ["ironman", "spiderman", "batman","heman","thor"];
-console.log(heros);
+// let heros = ["ironman", "spiderman", "batman","heman","thor"];
+// console.log(heros);
 
-let marks = [12,14,15,16,19]; //(out of 20)
-console.log(marks);
+// let marks = [12,14,15,16,19]; //(out of 20)
+// console.log(marks);
 
-let info = ["rahul",12,"Delhi"];
-console.log(info);
+// let info = ["rahul",12,"Delhi"];
+// console.log(info);
+
+// => array indices(index)
+
+let marks = [12,14,15,10,18];
+console.log(marks[0]);
+marks[3]=0;
+console.log(marks); //(here marks values is mutable means changeble so upper line changes happens.)
