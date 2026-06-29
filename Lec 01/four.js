cef79931d73d6989adbf1fc3e14c6a17dd4ab695
Raj Upala <rajupala005@gmail.com>
2026-06-29 11:19:33 +0530

    do it some final qs

--- Lec 01/four.js
+++ Lec 01/four.js
@@ -102,3 +102,17 @@
 
 //replce Element
 // num.splice(4,1,55);
+
+// Qs1
+
+// let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+// console.log(companies);
+
+// companies.shift();
+// console.log(companies);
+
+// companies.splice(2,1,"Ola");
+// console.log(companies);
+
+// companies.push("Amazon");
+// console.log(companies);
