2a33015760c93f65eecabcc233d3e9b9b11b3086
Raj Upala <rajupala005@gmail.com>
2026-06-30 09:21:41 +0530

    learn about functions

---
+++ Js lec/five.js
@@ -0,0 +1,27 @@
+// functions 
+
+// function myFunction() { // myFunction is my function name. Here my function print the writen line. 
+//     console.log("Welcome to apna college.");
+//     console.log("We are learning Js :) ");
+// }
+
+// myFunction(); // this way we call our function.
+
+// function myFun(msg){ // msg is my input and it's called as a parameters.
+//     console.log(msg);
+// }
+
+// myFun("I love Js."); // here is my argument.
+
+// using function 2 number sum do it.
+
+// function sum(x,y){  // x,y is our input and it's called as a parameters. REMEMBER -> This x,y parameters are alive in the block of scope {}.
+//     s = x + y;
+//     // console.log(s);
+//     return s; // it's return the value.(only one => if in array or anything  else is contain more value so its return it)
+// }
+
+// let finalAns = sum(5,5); // here i passess my argument. 
+// console.log(finalAns);
+
+//
