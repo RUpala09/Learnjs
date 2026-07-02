531d15318fa131c9eee9636691bf85d7628e1b97
Raj Upala <rajupala005@gmail.com>
2026-07-02 09:07:40 +0530

    add new js concepts

---
+++ Js lec/six.js
@@ -0,0 +1,19 @@
+//window object
+
+// console.log("hello");
+// window.console.log("hello2");
+
+//document object (it's window object's sub part)
+
+// console.log(window.document); // we get html files.
+// console.log(document); //we get all methods and property.
+
+// console.dir(window.document); // it's used to print document objects
+// console.log(window.document); // its use to print document elements.
+
+// DOM (document object model)
+
+console.dir(document.body); // we get all body tag objects.
+console.log(document.body); // we get all body elements.
+
+
