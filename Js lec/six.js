252e55700edfc27383e2e6d9fa01a509508c3c03
Raj Upala <rajupala005@gmail.com>
2026-07-03 11:24:45 +0530

    learn dom manipulation and properties

--- Js lec/six.js
+++ Js lec/six.js
@@ -20,6 +20,87 @@
  
 // document.body.childNodes[3].innerText = "abcd"; // written text change and become abcd with the help of js
 
+//  => DOM manipulation
+
+// 1) Using id we acces our elemnet.
+
+// let myId = document.getElementById("mainHeading"); 
+// console.dir(myId);
+
+// 2) using class
+
+// let myclass = document.getElementsByClassName("headingclass");
+// console.dir(myclass);  // its return the htmlcollection
+
+// 3) using tag name
+
+// let para = document.getElementsByTagName("p");
+// console.dir(para);
+
+// 4) using query selector (using query selector we acces one or all type of class tags and id.)
+
+ // mostly used this  query selector.....
+ 
+// let para = document.querySelector("p"); // it's gave only one things.
+// let para = document.querySelectorAll("p"); // it's return all paragraphs.
+// console.dir(para); 
+
+// -> for class
+
+// let firstEl = document.querySelector(".headingclass"); 
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll(".headingclass"); 
+// console.dir(allEl); 
+
+// -> for id
+
+// let myid = document.querySelector("#mainHeading");
+// console.dir(myid);
+
+ 
+// -> properties (we can access and change our values using this properties.)
+
+// let myid = document.querySelector("#mainHeading");
+// // console.dir(myid);
+// console.log(myid.tagName); //write in console and see the tagname.
+
+// let fruit = document.querySelector("div");
+// console.dir(fruit);
+// console.dir(fruit.innerText); // only text come
+// console.dir(fruit.innerHTML); // html tags and text come.
+
+// console.dir(fruit.innerText = "abcd");
+// console.dir(fruit.innerHTML = "<i>aabbccdd</i>");
+ 
+// => this way we change out values using innertext & innerhtml
+
+// let heading = document.querySelector("h1");
+// console.dir(heading);
+// console.dir(heading.innerText = "new heading"); // change my text old heading to new heading.
+// console.dir(heading.innerHTML = "<i>new heading</i>");  // come in italic way and also seen html changes in web browsers html file.
+
+// => textcontent is work as a innertext but its shows hidden elements,
+
+// let heading = document.querySelector("h1");
+// console.dir(heading.innerText); // not visible in this
+// console.dir(heading.textContent); // i seen it my hidden text.
+
+// => extra
+
+// let child = document.querySelector("h1").childNodes;
+// console.dir(child); // we get  h1 tag childernode list.
+
+// pr 1
+
+
+
+
+
+
+
+
+
 
 
 
